46d5f4d01c051e97180b01a6d7dc6715f8177437
Lindy <daniel.lindgren@abilia.se>
2025-10-15 12:28:13 +0200

    make time warp work

--- app/src/main/java/com/piggyapps/timewarper/MainActivity.kt
+++ app/src/main/java/com/piggyapps/timewarper/MainActivity.kt
@@ -1,14 +1,6 @@
 package com.piggyapps.timewarper
 
-import android.app.AlarmManager
-import android.app.admin.DevicePolicyManager
-import android.content.ComponentName
-import android.content.Context
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.provider.Settings
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.Crossfade
@@ -28,13 +20,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-
 import com.piggyapps.timewarper.ui.theme.TimeWarperTheme
 import kotlinx.coroutines.delay
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+import android.os.SystemClock
+import androidx.compose.runtime.collectAsState
+import kotlinx.coroutines.flow.MutableStateFlow
+
 class MainActivity : ComponentActivity() {
+    private val displayedTimeMillis = MutableStateFlow(System.currentTimeMillis())
+    private var lastRealtimeWhenPaused: Long = SystemClock.elapsedRealtime()
+    private var timeWarpFactor: Double = 10.0
+
+    override fun onResume() {
+        super.onResume()
+        val realtimeElapsedDuringPause = SystemClock.elapsedRealtime() - lastRealtimeWhenPaused
+        displayedTimeMillis.value += (realtimeElapsedDuringPause * timeWarpFactor).toLong()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lastRealtimeWhenPaused = SystemClock.elapsedRealtime()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,22 +53,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             TimeWarperTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    CurrentTimeDisplay()
+                    val currentDisplayedTime by displayedTimeMillis.collectAsState()
+                    CurrentTimeDisplay(displayedTimeMillis = currentDisplayedTime)
+                }
+
+                LaunchedEffect(Unit) {
+                    while (true) {
+                        updateDisplayedTime()
+                        delay(10L)
+                    }
                 }
             }
         }
     }
+
+
+    private fun updateDisplayedTime() {
+        displayedTimeMillis.value += 10L;
+    }
 }
 
 @Composable
-fun CurrentTimeDisplay(modifier: Modifier = Modifier) {
+fun CurrentTimeDisplay(modifier: Modifier = Modifier, displayedTimeMillis: Long) {
     var currentTime by remember { mutableStateOf("") }
 
-    LaunchedEffect(Unit) {
-        while (true) {
-            currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
-            delay(1000L)
-        }
+    LaunchedEffect(displayedTimeMillis) {
+        val fakedTime = LocalDateTime.ofEpochSecond(displayedTimeMillis / 1000, 0, java.time.ZoneOffset.ofHours(2))
+        currentTime = fakedTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"))
     }
 
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
